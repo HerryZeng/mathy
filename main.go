f8b6b3a6d96813386a2f0418196f1ec9777900a5
曾栋梁 <zengdongliang@i-yin.com.cn>
2020-08-26 11:53:02 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/HerryZeng/mathy"
+)
+func main() {
+	fmt.Println(mathy.Fib(10))
+	fmt.Println(mathy.Fact(10))
+}
---
+++ mathy.go
@@ -0,0 +1,29 @@
+package mathy
+
+// 函数名大写,其它的包者可以看得见
+func Fib(n int) int64 {
+	if n <= 1 {
+		return 1
+	}
+
+	var s = make([]int64, n+1)
+	s[0] = 1
+	s[1] = 1
+
+	for i := 2; i <= n; i++ {
+		s[i] = s[i-1] + s[i-2]
+	}
+	return s[n]
+}
+
+func Fact(n int) int64{
+	if n <=1 {
+		return 1
+	}
+
+	var s int64 = 1
+	for i:=2; i<=n;i++ {
+		s *= int64(i)
+	}
+	return s
+}
